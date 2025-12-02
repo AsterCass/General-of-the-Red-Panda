152bf086eb47cc01d848e7bbfaeb5bf23f196694
AsterCasc <astercass@qq.com>
2025-12-02 17:47:19 +0800

    feat: support web

--- The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/data/UserGrudgeData.kt
+++ The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/data/UserGrudgeData.kt
@@ -1,12 +1,28 @@
 package com.astercasc.squid.thebookofgrudges.data
 
+import com.astercasc.squid.thebookofgrudges.constant.enums.GrudgeLevel
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GrudgeCell(
-    var id: String = "",
+    var id: Long = 0L,
     var title: String = "",
     var description: String = "",
-    var level : Int = 0,
+    var level : GrudgeLevel = GrudgeLevel.ONE,
     var referCount : Int = 0,
+    var tags : List<GrudgeTag> = listOf(),
+    var obj : GrudgeObj? = null,
+    var createTime : Long = 0L,
+)
+
+@Serializable
+data class GrudgeObj(
+    var id: Long = 0L,
+    var name: String = "",
+)
+
+@Serializable
+data class GrudgeTag(
+    var id: Long = 0L,
+    var name: String = "",
 )
