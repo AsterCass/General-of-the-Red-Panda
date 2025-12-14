33a9efa9ff6ae02f69e45a15d23f98988eca3e94
AsterCasc <astercass@qq.com>
2025-12-14 22:49:20 +0800

    feat: support new tag & obj i18n

--- The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/NewGrudgeObject.kt
+++ The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/NewGrudgeObject.kt
@@ -6,13 +6,24 @@ import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.astercasc.squid.thebookofgrudges.data.DataStorageManager
 import com.astercasc.squid.thebookofgrudges.data.addNewObj
 import com.astercasc.squid.thebookofgrudges.data.model.GlobalDataModel
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
+import thebookofgrudges.composeapp.generated.resources.Res
+import thebookofgrudges.composeapp.generated.resources.add_obj_name
+import thebookofgrudges.composeapp.generated.resources.add_obj_title
+import thebookofgrudges.composeapp.generated.resources.add_obj_title_cancel
+import thebookofgrudges.composeapp.generated.resources.add_obj_title_confirm
+import thebookofgrudges.composeapp.generated.resources.error_name_empty
 
 @Composable
 fun NewGrudgeObject(
@@ -21,6 +32,8 @@ fun NewGrudgeObject(
     val newObjectName = rememberTextFieldState("")
     val dataStorageManager: DataStorageManager = koinInject()
     val globalDataModel: GlobalDataModel = koinInject()
+
+    var emptyName by remember { mutableStateOf(false) }
 
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
@@ -37,7 +50,7 @@ fun NewGrudgeObject(
                     horizontalArrangement = Arrangement.Center,
                 ) {
                     Text(
-                        text = "进入《死亡笔记》待审人员名单",
+                        text = stringResource(Res.string.add_obj_title),
                         style = MaterialTheme.typography.bodyLarge
                     )
                 }
@@ -49,31 +62,40 @@ fun NewGrudgeObject(
                     )
                 ) {
                     OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth().height(58.dp),
+                        modifier = Modifier.fillMaxWidth().height(76.dp),
                         state = newObjectName,
                         lineLimits = TextFieldLineLimits.SingleLine,
-                        label = { Text("todo 新建记仇对象") },
-                        placeholder = { Text("todo 记仇对象占位符") },
+                        label = { Text(stringResource(Res.string.add_obj_name)) },
                         shape = RoundedCornerShape(6.dp),
+                        isError = emptyName,
+                        supportingText = {
+                            if (emptyName)
+                                Text(stringResource(Res.string.error_name_empty))
+                        },
                     )
                 }
 
 
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
 
                     Button(
                         modifier = Modifier.weight(1f),
                         onClick = {
+                            if (newObjectName.text.isEmpty()) {
+                                emptyName = true
+                                return@Button
+                            }
+                            emptyName = false
                             addNewObj(globalDataModel, dataStorageManager, newObjectName.text.toString())
                             onDismissRequest()
                         },
                         shape = RoundedCornerShape(6.dp),
 
                         ) {
-                        Text("记仇标记！")
+                        Text(stringResource(Res.string.add_obj_title_confirm))
                     }
 
                     OutlinedButton(
@@ -81,7 +103,7 @@ fun NewGrudgeObject(
                         onClick = onDismissRequest,
                         shape = RoundedCornerShape(6.dp),
                     ) {
-                        Text("再观察观察")
+                        Text(stringResource(Res.string.add_obj_title_cancel))
                     }
                 }
             }
--- The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/NewGrudgeTag.kt
+++ The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/NewGrudgeTag.kt
@@ -5,14 +5,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.astercasc.squid.thebookofgrudges.data.DataStorageManager
 import com.astercasc.squid.thebookofgrudges.data.addNewTag
 import com.astercasc.squid.thebookofgrudges.data.model.GlobalDataModel
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
+import thebookofgrudges.composeapp.generated.resources.*
 
 @Composable
 fun NewGrudgeTag(
@@ -22,6 +24,8 @@ fun NewGrudgeTag(
     val newTagName = rememberTextFieldState("")
     val dataStorageManager: DataStorageManager = koinInject()
     val globalDataModel: GlobalDataModel = koinInject()
+
+    var emptyName by remember { mutableStateOf(false) }
 
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
@@ -38,7 +42,7 @@ fun NewGrudgeTag(
                     horizontalArrangement = Arrangement.Center,
                 ) {
                     Text(
-                        text = "todo 插入tag标题",
+                        text = stringResource(Res.string.add_tag_title),
                         style = MaterialTheme.typography.bodyLarge
                     )
                 }
@@ -50,31 +54,40 @@ fun NewGrudgeTag(
                     )
                 ) {
                     OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth().height(58.dp),
+                        modifier = Modifier.fillMaxWidth().height(76.dp),
                         state = newTagName,
                         lineLimits = TextFieldLineLimits.SingleLine,
-                        label = { Text("todo 新建tag对象") },
-                        placeholder = { Text("todo 记仇tag占位符") },
+                        label = { Text(stringResource(Res.string.add_tag_name)) },
                         shape = RoundedCornerShape(6.dp),
+                        isError = emptyName,
+                        supportingText = {
+                            if (emptyName)
+                                Text(stringResource(Res.string.error_name_empty))
+                        },
                     )
                 }
 
 
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
 
                     Button(
                         modifier = Modifier.weight(1f),
                         onClick = {
+                            if (newTagName.text.isEmpty()) {
+                                emptyName = true
+                                return@Button
+                            }
+                            emptyName = false
                             addNewTag(globalDataModel, dataStorageManager, newTagName.text.toString())
                             onDismissRequest()
                         },
                         shape = RoundedCornerShape(6.dp),
 
                         ) {
-                        Text("创建tag")
+                        Text(stringResource(Res.string.add_tag_title_confirm))
                     }
 
                     OutlinedButton(
@@ -82,7 +95,7 @@ fun NewGrudgeTag(
                         onClick = onDismissRequest,
                         shape = RoundedCornerShape(6.dp),
                     ) {
-                        Text("取消")
+                        Text(stringResource(Res.string.add_tag_title_cancel))
                     }
                 }
             }
