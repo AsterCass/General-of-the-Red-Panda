47be227db1c1a87317275e71e7e6d4d4546ac2a4
AsterCasc <astercass@qq.com>
2025-12-15 18:29:15 +0800

    feat: support pop navigation icon

--- The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/SystemComponents.kt
+++ The Book of Grudges/composeApp/src/commonMain/kotlin/com/astercasc/squid/thebookofgrudges/ui/components/SystemComponents.kt
@@ -1,40 +1,65 @@
 package com.astercasc.squid.thebookofgrudges.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.resources.stringResource
 import thebookofgrudges.composeapp.generated.resources.Res
 import thebookofgrudges.composeapp.generated.resources.system_confirm_cancel
 import thebookofgrudges.composeapp.generated.resources.system_confirm_ok
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, InternalVoyagerApi::class)
 @Composable
 fun MainAppBar(
     title: String,
 ) {
+
+    val navigator = LocalNavigator.currentOrThrow
+
     Surface(
         color = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
     ) {
-        Row(
+
+
+        Box(
             modifier = Modifier.windowInsetsPadding(TopAppBarDefaults.windowInsets).fillMaxWidth()
                 .padding(horizontal = 3.dp, vertical = 6.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
         ) {
 
-            Text(
-                text = title, style = MaterialTheme.typography.headlineSmall
-            )
+            if (navigator.canPop) {
+                Box(
+                    modifier = Modifier.align(Alignment.CenterStart).padding(start = 12.dp),
+                ) {
+                    Icon(
+                        modifier = Modifier.size(24.dp).clickable(onClick = { navigator.pop() }),
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                    )
+                }
+            }
 
+            Box(
+                modifier = Modifier.align(Alignment.Center),
+            ) {
+                Text(
+                    text = title, style = MaterialTheme.typography.headlineSmall
+                )
+            }
         }
+
     }
 }
 
