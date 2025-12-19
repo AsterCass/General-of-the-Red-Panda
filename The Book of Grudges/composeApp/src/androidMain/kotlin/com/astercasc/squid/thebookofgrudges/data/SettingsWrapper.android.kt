17a3dc1c86f51a6df3edf7e6e1983519aa296b52
AsterCasc <astercass@qq.com>
2025-12-19 22:21:25 +0800

    fix: change function of get context

--- The Book of Grudges/composeApp/src/androidMain/kotlin/com/astercasc/squid/thebookofgrudges/data/SettingsWrapper.android.kt
+++ The Book of Grudges/composeApp/src/androidMain/kotlin/com/astercasc/squid/thebookofgrudges/data/SettingsWrapper.android.kt
@@ -3,10 +3,13 @@ package com.astercasc.squid.thebookofgrudges.data
 import android.content.Context
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 
-actual class SettingsWrapper(private val context: Context) {
+actual class SettingsWrapper() : KoinComponent {
 
+    private val context: Context by inject()
 
     actual fun createSettings(): Settings {
         return SharedPreferencesSettings(
--- The Book of Grudges/composeApp/src/androidMain/kotlin/com/astercasc/squid/thebookofgrudges/di/CommonModule.android.kt
+++ The Book of Grudges/composeApp/src/androidMain/kotlin/com/astercasc/squid/thebookofgrudges/di/CommonModule.android.kt
@@ -1,14 +1,11 @@
 package com.astercasc.squid.thebookofgrudges.di
 
-import com.astercasc.squid.thebookofgrudges.MainActivity
 import com.astercasc.squid.thebookofgrudges.data.SettingsWrapper
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
     single {
-        SettingsWrapper(
-            context = MainActivity.mainContext!!,
-        ).createSettings()
+        SettingsWrapper().createSettings()
     }
 }
