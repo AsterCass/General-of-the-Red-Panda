3348f0a135fc8581fa7c0ad2bcbc94c809e9b0da
AsterCasc <astercass@qq.com>
2026-04-16 15:48:19 +0800

    refactor: output no lock

--- Build Home AI Agent/src/meow_ai_agent/audio/output.py
+++ Build Home AI Agent/src/meow_ai_agent/audio/output.py
@@ -27,8 +27,6 @@ class AudioOutput:
 
         # 当前要播放的文本
         self.text_queue = Queue(maxsize=10)
-        # 同步锁
-        self._lock = threading.Lock()
         # 播放线程常驻
         self._thread = threading.Thread(
             target=self._speak_worker,
@@ -38,29 +36,24 @@ class AudioOutput:
 
     def _speak_worker(self):
         while True:
-            with self._lock:
-                if self.text_queue.empty():
-                    time.sleep(0.1)
-                    continue
-                current_text = self.text_queue.get()
-                try:
-                    for audio_chunk, sr, timing in self.model.generate_custom_voice_streaming(
-                            text=current_text,
-                            language="Chinese",
-                            speaker="Serena",
-                            non_streaming_mode=False,
-                    ):
-                        self.thisPlay(audio_chunk, sr)
-                    self.thisPlay.end_segment()
-                except Exception as e:
-                    logger.error(f"播放异常: {e}")
+            current_text = self.text_queue.get()
+            try:
+                for audio_chunk, sr, timing in self.model.generate_custom_voice_streaming(
+                        text=current_text,
+                        language="Chinese",
+                        speaker="Serena",
+                        non_streaming_mode=False,
+                ):
+                    self.thisPlay(audio_chunk, sr)
+
+                self.thisPlay.end_segment()
+
+            except Exception as e:
+                logger.error(f"播放异常: {e}")
 
     def speak(self, text: str):
         logger.info(f"进入输出队列文本: {text}")
-        with self._lock:
-            try:
-                self.text_queue.put(text, block=False)
-            except Full:
-                logger.warning("音频输出堆栈满")
-            except Exception as e:
-                logger.error(f"音频输出堆栈异常: {e}")
+        try:
+            self.text_queue.put(text, block=False)
+        except Full:
+            logger.warning("音频输出堆栈满")
